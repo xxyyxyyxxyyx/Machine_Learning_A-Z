9ff352290c7eaeb9003f49c21323e55caf05e9ed
Sunil Limbu <xxyyxyyxxyyx@gmail.com>
2017-10-15 22:24:57 +0800

    commit

--- Part 1 - Data Preprocessing/my.R
+++
@@ -1,9 +0,0 @@
-# Importing datasets
-dataset = read.csv('Data.csv')
-# Insert missing data
-dataset$Age = ifelse(is.na(dataset$Age),
-                     ave(dataset$Age, FUN = function(x)mean(x, na.rm = TRUE)),
-                     dataset$Age)
-dataset$Salary = ifelse(is.na(dataset$Salary),
-                        ave(dataset$Salary, FUN = function(x)mean(x, na.rm = TRUE)),
-                        dataset$Salary)
