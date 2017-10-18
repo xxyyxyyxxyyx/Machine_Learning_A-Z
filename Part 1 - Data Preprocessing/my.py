0bb8b213ba873cbede9c4cfd301028d4506e2f58
Administrator <xxyyxyyxxyyx@gmail.com>
2017-10-18 18:11:11 +0800

    Commit

--- Part 1 - Data Preprocessing/my.py
+++ Part 1 - Data Preprocessing/my.py
@@ -23,3 +23,7 @@ X= onehotencoder.fit_transform(X).toarray()
 #Encoding purchased categorical colmun into number
 labelencoder_Y = LabelEncoder()
 Y = labelencoder_Y.fit_transform(Y)
+
+#Splitting the data into training and test set
+from sklearn.cross_validation import train_test_split
+X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size = 0.2,random_state = 0)
